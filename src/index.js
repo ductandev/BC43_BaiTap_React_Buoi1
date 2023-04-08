cff0426a65c81e1d5b5324460ae6a3e22179ca29
Nguyen Duc Tan <nguyenductan998@gmail.com>
2023-04-08 22:29:44 +0700

    baitap_reatc_buoi1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import BaiTapThucHanhLayout from './Component/BaiTapThucHanhLayout/BaiTapThucHanhLayout';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div>
-    "Hello cybersoft"
+    <BaiTapThucHanhLayout/>
   </div>
 );
 
